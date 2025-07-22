841b4e7a5ce65d02cb7babd5d596f24ead13b36f
Quan Nguyen <sofh1725@gmail.com>
2025-07-22 22:31:42 +0700

    check

--- tank_agent_install.sh
+++ tank_agent_install.sh
@@ -31,7 +31,7 @@ fi
 echo "📁 Fetching Agent_For_Server files..."
 sudo rm -rf "$INSTALL_DIR"
 rm -rf "$REPO_DIR"
-git clone --branch master https://github.com/Nguyen-Nhut-Minh-Quan/Proxmox-Agent-Project.git "$REPO_DIR" || {
+sudo git clone --branch master https://github.com/Nguyen-Nhut-Minh-Quan/Proxmox-Agent-Project.git "$REPO_DIR" || {
   echo "❌ Git clone failed. Aborting."; exit 1;
 }
 #!/bin/bash
