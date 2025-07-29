0d5be4fa468a9a7cef84d2afdccc459f24d82527
Quan Nguyen <sofh1725@gmail.com>
2025-07-29 14:25:54 +0700

    check

---
+++ tank_agent_remove.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "remove service...."
+
+SERVICE_NAME="tank_agent.service"
+TIMER_NAME="tank_agent.timer"
+
+sudo systemctl stop "$TIMER_NAME"
+sudo systemctl stop "$SERVICE_NAME"
+
+# Disable services to prevent auto-start
+sudo systemctl disable "$TIMER_NAME"
+sudo systemctl disable "$SERVICE_NAME"
+
+sudo rm "/etc/systemd/system/$SERVICE_NAME"
+sudo rm "/etc/systemd/system/$TIMER_NAME" 
+
+echo "finishing remove"
