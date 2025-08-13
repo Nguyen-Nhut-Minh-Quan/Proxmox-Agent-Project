6b36d9e84fc4d3b89ed431094ba8c382d532d640
Quan Nguyen <sofh1725@gmail.com>
2025-08-13 12:50:52 +0700

    check

--- proxmox_agent_update.sh
+++ proxmox_agent_update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "🔄 Starting Proxmox-Agent update..."
+echo "🔄 Starting Proxmox-Agent..."
 sudo systemctl stop proxmox_agent.timer
 sudo systemctl stop proxmox_agent.service
 INSTALL_DIR="/opt/Proxmox-Agent"
