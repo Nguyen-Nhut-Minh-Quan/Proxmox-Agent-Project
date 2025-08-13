23133c890a5e1b9bec70a9aa20a2390bf05f7977
Quan Nguyen <sofh1725@gmail.com>
2025-08-13 16:46:13 +0700

    check

--- Agent_For_Server/proxmox_agent.c
+++ Agent_For_Server/proxmox_agent.c
@@ -624,12 +624,12 @@ void insert_cpu_usage_via_api() // Modified signature
                        cores,
                        loadavg,
                        timestamp);
-
+    printf("Debug for fast api %s",fastapi_base_url);
     if (len < 0 || len >= sizeof(json_payload)) {
         fprintf(stderr, "[ERROR] JSON payload for CPU usage too large or error occurred.\n");
     } else {
         char url_buffer[256];
-        snprintf(url_buffer, sizeof(url_buffer), "%s/physical-server/cpu-usage/", fastapi_base_url);
+        snprintf(url_buffer, sizeof(url_buffer), "/physical-server/cpu-usage/%s", fastapi_base_url);
         post_json_to_api(url_buffer, json_payload);
     }
     printf("[DEBUG] Completed insert_cpu_usage\n");
